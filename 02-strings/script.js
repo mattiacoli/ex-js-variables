dd54919be48e2ffa70adb47a47a41a4a73777171
mattiacoli <mattia.coli@aol.com>
2025-02-07 15:11:04 +0100

    solve excercise 1 and 2 in strings part

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,21 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+  const nome = 'Mattia'
 // Stampa il valore di nome nella console.
+  console.log(nome);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+let number;
+let text;
+let bool;
 // un numero, una stringa e un valore booleano.
+number = 11;
+text = 'ciao';
+bool = 0;
 // Stampa tutti i valori nella console.
+console.log(number, text, bool);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
